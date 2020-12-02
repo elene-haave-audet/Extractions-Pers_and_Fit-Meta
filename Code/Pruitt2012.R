6c66c4968997bbfb8c6ef7c563602e109ffbcc75
Elene Haave Audet <haaveaud@ualberta.ca>
2020-12-02 14:44:34 -0700

    format Pruitt 2012

--- Code/Pruitt2012.R
+++ Code/Pruitt2012.R
@@ -1,20 +1,18 @@
-#================================================
 # Variance Partitioning for Pruitt 2012, Rec 204
 # Elene Haave Audet, Mar 23, 2020
 # Day 12 pandemic
-#================================================
 
-# 1) Set working directory to data location
-setwd("G:/Shared drives/Personality & Fitness Meta-Analysis/R_Pers&FitExtractions/Data")
-
-# 2) Load libraries
+# Load libraries====
 library(tidyverse)
 library(lme4)
 library(MuMIn)
 library(MCMCglmm)
 library(rptR)
+library(here)
 
-# 3) Load Data
-#no individual IDs given, 
-#therefore cannot partition variation at among level
+# Set wd
+dir<-here()
 
+# Load Data
+# no individual IDs given, 
+# therefore cannot partition variation at among level
