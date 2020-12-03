94689fefd47a62f354f60b0e58fd6299ce1e330f
Elene Haave Audet <haaveaud@ualberta.ca>
2020-12-03 11:54:56 -0700

    edit annotations in Belgrad 2018

--- Code/Belgrad2018.R
+++ Code/Belgrad2018.R
@@ -10,8 +10,10 @@ dir<-here()
 # Load data====
 Belgrad<-read.csv("Data/Belgrad2018.csv")
 
+# note from authors:
+# behaviour 1= extremely bold, 0= extremely shy
 BelF<-subset(Belgrad, gender=="f")
-cor.test(BelF$behavior, BelF$censor) #r=-0.05060336 (switch sign to reflect boldness)
+cor.test(BelF$behavior, BelF$censor) # high values= high boldness, sign does not need to be switched
 
 BelM<-subset(Belgrad, gender=="m")
-cor.test(BelM$behavior, BelM$censor) #r=-0.1379683 (switch sign to reflect boldness)
+cor.test(BelM$behavior, BelM$censor) # high values= high boldness, sign does not need to be switched
