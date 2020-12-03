113d64f5eeedd7a90de61cdb6d1c49553ad26b65
Elene Haave Audet <haaveaud@ualberta.ca>
2020-12-03 16:40:34 -0700

    create Abbey-Lee 2018

---
+++ Code/Abbey-Lee2018.R
@@ -0,0 +1,34 @@
+# Abbey-Lee 2018 Rec 5
+# Variance partitioning
+
+# Load libraries====
+library(here)
+library(tidyverse)
+library(MCMCglmm)
+library(rptR)
+
+# Set wd====
+dir<-here()
+
+# Load data====
+data<-read.csv("Data/AbbeyLee2018.csv")
+
+ind=data %>% 
+  group_by(MaleID) %>% 
+  summarize(Male=n_distinct(MaleID))
+
+# Models====
+m1<-rpt(Exploration~(1|MaleID), data=data, grname="MaleID")
+m1
+
+hist(data$Exploration) #can assume normality
+
+m3<-MCMCglmm(cbind(Exploration, EPY) ~ (trait-1), random = ~us(trait):MaleID ,rcov = ~us(trait):units, family = c("gaussian", "categorical"), data=data, prior =Prior3, verbose = FALSE,nitt=103000,thin=100,burnin=3000)
+plot(m3) #Maria's prior seems to work okay for this model
+
+c1 <- posterior.cor(m3$VCV[,1:4])
+round(apply(c1,2,mean),2)
+round(apply(c1,2, quantile, c(0.025, 0.975)),2)
+c2 <- posterior.cor(m3$VCV[,5:8])
+round(apply(c2,2,mean),2)
+round(apply(c2,2, quantile, c(0.025, 0.975)),2)
